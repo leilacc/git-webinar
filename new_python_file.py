3808ac2a716b63696f537c88c14fc39083a39d54
Leila Chan Currie <leilacc@leilacc-macbookpro.roam.corp.google.com>
2016-04-07 18:09:27 -0700

    Added simple main

---
+++ new_python_file.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+  print "Hello NYUAD!" # TODO: change to say Hello to someone else
+  my_favourite_colour = "red" # TODO: change this to a better colour!
+  print "My favourite colour is " + my_favourite_colour
+
+  # TODO: add another print statement
